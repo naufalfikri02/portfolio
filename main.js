7981b015b707f049fe36a01f27710705d186e1af
naufalfikri0206 <Naufalfikri0206@gmail.com>
2023-10-23 15:08:29 +0700

    ubah foto

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function toggleDetail(e) {
     const target = $(e.target)
 
-    if($(target).hasClass("active")) {
+    if ($(target).hasClass("active")) {
         $(target).html("More Info").removeClass("active")
     } else {
         $(target).html("Less Info").addClass("active")
     }
-    
+
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
     $(detail).slideToggle()
@@ -17,7 +17,7 @@ function onFormSubmit(e) {
     const subject = $("#inp_subject")
     const message = $("#inp_message")
 
-    if(!$(email).val()) {
+    if (!$(email).val()) {
         alert("Email is required")
     } else if (!$(subject).val()) {
         alert("Subject is required")
